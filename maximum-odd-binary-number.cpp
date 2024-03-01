75b3729a1c3f9b33c623010684210493166726de
Ashutosh <ashutosh.kumar.patel.2003@gmail.com>
2024-03-01 09:56:07 +0530

    Initial Commit

---
+++ maximum-odd-binary-number.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        unordered_map<char, int> cCount;
+        string ans;
+        for (char c : s) {
+            cCount[c]++;
+        }
+        int count1 = cCount['1'];
+        int count0 = cCount['0'];
+
+        for (int i = 0; i < count1 - 1; i++) {ans.push_back('1');}
+        for (int i = 0; i < count0; i++) {ans.push_back('0');}
+        ans.push_back('1');
+
+        return ans;
+    }
+};
+
+
+int main() {
+    string s = "0110";
+    Solution sol;
+    cout << sol.maximumOddBinaryNumber(s) << endl;
+    return 0;
+}
