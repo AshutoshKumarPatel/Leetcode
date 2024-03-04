60e562303d6f3b26db0dba7615ba325f4b113bff
Ashutosh <ashutosh.kumar.patel.2003@gmail.com>
2024-03-04 10:32:24 +0530

    Runtime: 3ms Memory: 3.12MB

---
+++ remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* first = dummy;
+        ListNode* second = dummy;
+
+        for (int i = 0; i <= n; ++i) {
+            first = first->next;
+        }
+
+        while (first != nullptr) {
+            first = first->next;
+            second = second->next;
+        }
+
+        ListNode* temp = second->next;
+        second->next = second->next->next;
+        delete temp;
+
+        return dummy->next;
+    }
+};
+
+int main() {
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    Solution sol;
+    int n = 2;
+    ListNode* modifiedHead = sol.removeNthFromEnd(head, n);
+
+    ListNode* current = modifiedHead;
+    while (current != nullptr) {
+        cout << current->val << " ";
+        current = current->next;
+    }
+    cout << endl;
+    return 0;
+}
