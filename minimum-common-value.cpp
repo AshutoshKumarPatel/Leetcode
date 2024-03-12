15750f4cb5177d4a258dce32b5e28aa76476923a
Ashutosh <ashutosh.kumar.patel.2003@gmail.com>
2024-03-12 13:29:30 +0530

    Runtime: 58ms Memory: 52.9MB

---
+++ minimum-common-value.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int common = INT_MAX;
+        int i = 0, j = 0;
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                common = nums1[i];
+                break;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return common != INT_MAX ? common : -1;
+    }
+};
+
+int main() {
+    vector<int> nums1 = {1,2,3};
+    vector<int> nums2 = {2,4};
+    Solution sol;
+    cout << sol.getCommon(nums1, nums2) << endl;
+    return 0;
+}
